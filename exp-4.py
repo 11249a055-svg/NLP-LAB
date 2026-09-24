ed4eba4ede9fdbcc8d56663dceded0fd937dc08b
11249a055-svg <11249a055@kanchiuniv.ac.in>
2026-09-24 16:08:10 +0530

    Add files via upload

---
+++ exp-4.py
@@ -0,0 +1,7 @@
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+ps = PorterStemmer()
+new_text = "It is very important to be pythonly while you are pythoning with python, atleast once"
+words = word_tokenize(new_text)
+for w in words:
+    print(ps.stem(w))
